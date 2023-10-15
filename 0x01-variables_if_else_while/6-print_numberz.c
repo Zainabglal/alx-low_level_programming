1f7b04284607d5141d409e7b096b901033474b42
Zainabglal <zainabglal325@gmail.com>
2023-10-15 20:23:29 +0000

    print digits with putchar (48+)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: same as before with
+ * different method
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar (48+a);
+	putchar ('\n');
+	return (0);
+}
