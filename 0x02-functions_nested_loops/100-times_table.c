af0375014b953f60bb8dd1dce7aab1f2b47bb325
Zainabglal <zainabglal325@gmail.com>
2023-10-19 19:29:55 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,33 +9,32 @@ void print_times_table(int n)
 	int a, b;
 
 	if (n < 0 || n > 15)
-		_putchar('\0');
-	else
+		return;
+	for (a = 0; a <= n; a++)
 	{
-		for (a = 0; a <= n; a++)
+		for (b = 0; b <= n; b++)
 		{
-			for (b = 0; b <= n; b++)
-			{
-				if (b != 0)
-				{	_putchar(',');
-					_putchar(' ');
-				}
-				if ((a * b) <= 9 && b != 0)
-				{	_putchar(' ');
-					_putchar(' ');
-					_putchar((a * b) + '0');
-				}
-				else if ((a * b) <= 9 && b == 0)
-					_putchar((a * b) + '0');
-				if ((a * b) > 9 && (a * b) < 100)
-				{	_putchar(' ');
-					_putchar(((a * b) / 10) + '0');
-					_putchar(((a * b) % 10) + '0');
-				}
-				if ((a * b) >= 100)
-				{	_putchar(((a * b) / 100) + '0');
-					_putchar((((a * b) % 100) / 10) + '0');
-					_putchar(((a * b) % 10) + '0');
-				}}
+			if (b != 0)
+			{	_putchar(',');
+				_putchar(' ');
+			}
+			if ((a * b) <= 9 && b != 0)
+			{	_putchar(' ');
+				_putchar(' ');
+				_putchar((a * b) + '0');
+			}
+			else if ((a * b) <= 9 && b == 0)
+				_putchar((a * b) + '0');
+			if ((a * b) > 9 && (a * b) < 100)
+			{	_putchar(' ');
+				_putchar(((a * b) / 10) + '0');
+				_putchar(((a * b) % 10) + '0');
+			}
+			if ((a * b) >= 100)
+			{	_putchar(((a * b) / 100) + '0');
+				_putchar((((a * b) % 100) / 10) + '0');
+				_putchar(((a * b) % 10) + '0');
+			}}
 		_putchar('\n');
-		}}}
+		}
+}
