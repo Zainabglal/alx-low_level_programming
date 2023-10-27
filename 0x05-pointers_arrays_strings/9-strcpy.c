3532c1f1e824ccd7697388aaa91ffda70f2b72b6
Zainabglal <zainabglal325@gmail.com>
2023-10-27 17:05:34 +0000

    copy string fun

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string of a variable into another variable
+ * @dest: the destination of string to be copied in
+ * @src: the source of the string to be copied from
+ * Return: the destination value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
