b889e4065d7e6d10bb8d2378f29918280e46ff84
Zainabglal <zainabglal325@gmail.com>
2023-10-27 13:40:22 +0000

    reverse a string again

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the address of string to be reversed
+ * Return: none
+ */
+void rev_string(char *s)
+{
+	int len, count, temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	for (count = 0; count < len/2; count ++)
+	{
+		temp = s[count];
+		s[count] = s[len - count - 1];
+		s[len - count - 1] = temp;	
+	}
+}
