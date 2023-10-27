70754bc74274d2049089d5fdb1052ed050751861
Zainabglal <zainabglal325@gmail.com>
2023-10-27 15:18:20 +0000

    print the other half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,18 +13,9 @@ void puts_half(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	if (i % 2 != 0)
-	{
-		for ((j = (i  / 2 - 1)); j < i; j++)
-		{
-			_putchar(str[j]);
+	i += 1;
+	for (j = i  / 2 ; j < i; j++)
+		_putchar(str[j]);
 
-		}
-	}
-	else
-	{
-		for ((j = i / 2); j < i; j++)
-			_putchar(str[j]);
-	}
 	_putchar('\n');
 }
