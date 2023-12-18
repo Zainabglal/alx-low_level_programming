e0161ca54f3bd3c4e0654958a6876931a558fb6c
Zainabglal <zainabglal325@gmail.com>
2023-12-18 21:55:59 +0000

    string comparison and return the difference between ascii values

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	if (s1 > s2)
-		i = 15;
-	else if (s1 < s2)
-		i = -15;
-	else
-		i = 0;
-	return (i);
+	for (i = 0; s1[i] == s2[i] && (s1[i] || s2[i] != '\0'); i++)
+	{
+		continue;
+	}
+
+
+
+	return (s1[i] - s2[i]);
 }
