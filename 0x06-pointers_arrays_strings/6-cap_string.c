c679a04467c0792c161007a983dc51806cc1dd62
Zainabglal <zainabglal325@gmail.com>
2023-12-19 21:18:24 +0000

    capitalize the first char of a word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes the first character of words
+ * @s: string that will have it's first character capitalized
+ * Return: the capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i + 1] >= 97 && s[i + 1] <= 122)
+		{
+			if ((s[i] == '.' && s[i + 1] != ' ') || s[i] == '\t'
+					|| s[i] == '\n' || s[i] == ' ')
+				s[i + 1] = s[i + 1] - 32;
+			else
+				s[i + 1] = s[i + 1];
+		}
+		else
+			s[i + 1] = s[i + 1];
+	}
+	return (s);
+}
